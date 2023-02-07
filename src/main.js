13b7fe47a4bab86086dec829315dc09d723893c3
zgjswxcmq <939082952@qq.com>
2023-02-07 15:35:15 +0800

    update orderList's pagination

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import VueLazyLoad from 'vue-lazyload'
 import VueCookie from 'vue-cookie'
+import infiniteScroll from 'vue-infinite-scroll'
 // import './mock/api'
 Vue.use(VueLazyLoad, {
   loading: '/imgs/loading-svg/loading-bars.svg'
@@ -12,6 +13,7 @@ Vue.use(VueLazyLoad, {
 
 Vue.use(VueCookie)
 Vue.use(Message)
+Vue.use(infiniteScroll)
 
 Vue.prototype.$message = Message
 
--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -3,6 +3,8 @@
     <div class="wrapper">
       <div class="container">
         <div class="order-box">
+          <loading v-if="loading"></loading>
+          
           <div class="order" v-for="(order, index) in list" :key="index">
             <div class="order-title">
               <div class="item-info fl">
@@ -46,7 +48,21 @@
               </div>
             </div>
           </div>
+          <el-pagination
+            class="pagination"
+            v-if="paginationShow"
+            background
+            layout="prev, pager, next"
+            :pageSize="pageSize"
+            @current-change="handleChange"
+            :total="total">
+          </el-pagination>
+          <div class="load-more"
+            v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="100">
+            <img src="/imgs/loading-svg/loading-spinning-bubbles.svg" alt="" v-if="!busy">
+          </div>
         </div>
+        <no-data v-if="loading === false && list.length === 0"></no-data>
       </div>
     </div>
   </div>
@@ -54,22 +70,51 @@
 
 <script>
 import instance from "@/util/request";
+import Loading from "@/components/Loading.vue";
+import NoData from "@/components/NoData.vue";
+import { Pagination } from "element-ui";
 export default {
   name: "order-list",
   data() {
     return {
-      list: [],
+      list: [], //订单列表
+      loading: true,
+      pageSize: 10, //使用分页器时每页订单数
+      pageNum: 1, //使用分页器时当前页码
+      total: 0, //总订单数
+      paginationShow:false, //是否使用分页器
+      busy: true, //控制滚动加载
     };
   },
   mounted() {
     this.getOrderList();
   },
   methods: {
+    //获取订单列表
     getOrderList() {
-      instance.get("/orders").then((res) => {
-        this.list = res.list;
-      });
+      instance.get("/orders",{
+        params:{
+          pageSize: this.pageSize,
+          pageNum : this.pageNum
+        }
+      })
+      .then((res) => {
+        //this.list = res.list; //分页器时加载每页订单
+        //this.total = res.total //使用分页器时有知道所有订单数的需求，计算总页数
+        this.list = this.list.concat(res.list) //滚动加载时在后面增加订单
+        if(res.hasNextPage === true){
+          this.busy = false
+        }else{
+          this.busy = true
+        }
+        this.loading = false
+      })
+      .catch(()=>{
+        this.loading = false
+        this.$message.warning('订单列表加载失败')
+      })
     },
+    //从未支付前往支付页面
     goPay(orderId) {
       this.$router.push({
         path: "/order/pay",
@@ -78,7 +123,22 @@ export default {
         },
       });
     },
+    //使用分页器时，点击页码跳转
+    handleChange(pageNum){
+      this.pageNum = pageNum
+      this.getOrderList()
+    },
+    loadMore(){
+      this.busy = true
+      this.pageNum++
+      this.getOrderList()
+    }
   },
+  components:{
+    Loading,
+    NoData,
+    [Pagination.name] : Pagination
+  }
 };
 </script>
 
@@ -154,8 +214,7 @@ export default {
         background-color: #ff6600;
         border-color: #ff6600;
       }
-      .load-more,
-      .scroll-more {
+      .load-more {
         text-align: center;
       }
     }
